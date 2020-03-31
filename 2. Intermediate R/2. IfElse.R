bba29ba86f2b60831f95a3bf3c0aec70155158bc
ShreekantSaurabh <srikant12saurabh@gmail.com>
2020-03-31 02:21:13 +0200

    ifelse

---
+++ 2. Intermediate R/2. IfElse.R
@@ -0,0 +1,26 @@
+# Variables related to your last day of recordings
+medium <- "LinkedIn"
+num_views <- 14
+
+# Control structure for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+} else if (medium == "Facebook") {
+  print("Showing Facebook information")
+  # Add code to print correct string when condition is TRUE
+  
+} else {
+  print("Unknown medium")
+}
+
+# Control structure for num_views
+if (num_views > 15) {
+  print("You're popular!")
+} else if (num_views <= 15 & num_views > 10) {
+  print("Your number of views is average" )
+  # Add code to print correct string when condition is TRUE
+  
+} else {
+  print("Try to be more visible!")
+}
+
